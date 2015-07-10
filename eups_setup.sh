3685b3ddc6297c005743cf0fd3e52f81ab4e6a99
Felipe Menanteau <felipe@illinois.edu>
2015-07-09 22:27:37 -0500

    removing eups bash script

--- eups_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Setting up EUPS for Thumbnails"
-source $EUPS_DIR/desdm_eups_setup.sh
-
-# pre-reqs
-setup -r ~/DESDM-Code/devel/despyastro/trunk
-setup despydb 2.0.0+2
-setup fitsio 0.9.8rc1+0 
