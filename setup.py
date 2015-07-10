e62821b394a9a72142bde649618f9e9cbd3057fa
Felipe Menanteau <felipe@illinois.edu>
2015-07-10 10:20:07 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import distutils
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*.py") 
+
+# The main call
+setup(name='desthumbs',
+      version ='0.1.0',
+      license = "GPL",
+      description = "A python module to make FITS files cutouts/thumbnails for DES",
+      author = "Felipe Menanteau",
+      author_email = "felipe@illinois.edu",
+      packages = ['desthumbs'],
+      package_dir = {'': 'python'},
+      scripts =  ['bin/makeDESthumbs'],
+      data_files=[('ups',['ups/desthumbs.table'])
+                  ('etc',  glob.glob("etc/*.*"))
+                  ],
+      )
+
