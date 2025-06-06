02c1712a203c53c0a8ac39320839ace2cfeac477
Felipe Menanteau <felipe@illinois.edu>
2025-06-06 18:28:02 -0500

    More SQL queries

--- make_tables.sql
+++ make_tables.sql
@@ -12,5 +12,13 @@ create table Y6A2_COADD_FILEPATH as
   from prod.COADD c, prod.PROCTAG, prod.FILE_ARCHIVE_INFO f
          where prod.PROCTAG.TAG='Y6A2_COADD' and
 --           c.FILETYPE='coadd' and
-           c.PFW_ATTEMPT_ID=PROCTAG.PFW_ATTEMPT_ID and
+           c.PFW_ATTEMPT_ID=PROCTAG.PFW_ATTEMPT_ID and des_admin.Y6A2
            f.FILENAME=c.FILENAME;
+
+
+select i.FILENAME, i.BAND, i.FILETYPE, i.EXPTIME, i.NITE, i.EXPNUM, e.DATE_OBS from Y6A2_IMAGE i, Y6A2_EXPOSURE e
+  where ((i.CROSSRA0='N' AND (0.29782658 BETWEEN i.RACMIN and i.RACMAX) AND (0.029086056 BETWEEN i.DECCMIN and i.DECCMAX)) OR
+        (i.CROSSRA0='Y' AND (0.29782658 BETWEEN i.RACMIN-360 and i.RACMAX) AND (0.029086056 BETWEEN i.DECCMIN and i.DECCMAX)))
+        and i.EXPNUM=e.EXPNUM
+        and i.BAND='r'
+        and i.FILETYPE='red_immask';
