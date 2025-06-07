bc183acfe09bd5ea39917981e6f917cb68b68eb2
Felipe Menanteau <felipe@illinois.edu>
2025-06-07 11:55:16 -0500

    More table creation

--- make_tables.sql
+++ make_tables.sql
@@ -16,9 +16,26 @@ create table Y6A2_COADD_FILEPATH as
            f.FILENAME=c.FILENAME;
 
 
-select i.FILENAME, i.BAND, i.FILETYPE, i.EXPTIME, i.NITE, i.EXPNUM, e.DATE_OBS from Y6A2_IMAGE i, Y6A2_EXPOSURE e
+--- build table with info for finalcut files
+create table Y6A2_FINALCUT_FILEPATH as
+select i.FILENAME, f.PATH, i.BAND, i.EXPTIME, i.AIRMASS, i.FWHM, i.NITE, i.EXPNUM, i.CCDNUM, e.DATE_OBS, e.MJD_OBS,
+       i.CROSSRA0, i.RACMIN, i.RACMAX, i.DECCMIN, i.DECCMAX,
+       i.RAC1, i.RAC2, i.RAC3, i.RAC4, i.DECC1, i.DECC2, i.DECC3, i.DECC4
+ from Y6A2_IMAGE i, Y6A2_EXPOSURE e, Y6A2_FILE_ARCHIVE_INFO f
+ where  i.EXPNUM=e.EXPNUM
+        and i.FILENAME=f.FILENAME
+        and i.FILETYPE='red_immask';
+
+-- example of search near ra, dec
+select i.FILENAME, i.BAND, i.FILETYPE, i.EXPTIME, i.NITE, i.EXPNUM, e.DATE_OBS, e.MJD_OBS from Y6A2_IMAGE i, Y6A2_EXPOSURE e
   where ((i.CROSSRA0='N' AND (0.29782658 BETWEEN i.RACMIN and i.RACMAX) AND (0.029086056 BETWEEN i.DECCMIN and i.DECCMAX)) OR
         (i.CROSSRA0='Y' AND (0.29782658 BETWEEN i.RACMIN-360 and i.RACMAX) AND (0.029086056 BETWEEN i.DECCMIN and i.DECCMAX)))
         and i.EXPNUM=e.EXPNUM
         and i.BAND='r'
         and i.FILETYPE='red_immask';
+
+-- example of search near ra, dec
+select i.FILENAME, i.BAND, i.EXPTIME, i.NITE, i.EXPNUM, i.DATE_OBS, i.MJD_OBS from felipe.Y6A2_FINALCUT_FILEPATH i
+  where ((i.CROSSRA0='N' AND (0.29782658 BETWEEN i.RACMIN and i.RACMAX) AND (0.029086056 BETWEEN i.DECCMIN and i.DECCMAX)) OR
+  (i.CROSSRA0='Y' AND (0.29782658 BETWEEN i.RACMIN-360 and i.RACMAX) AND (0.029086056 BETWEEN i.DECCMIN and i.DECCMAX)))
+  and i.BAND='r';
